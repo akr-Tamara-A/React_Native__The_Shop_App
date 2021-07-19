e6cbe427c75371e639dcfb1ecfc5b9e128ee0632
Tamara A <akira-49-q@yandex.ru>
2021-07-19 13:47:55 +0300

    feat: add redux store

---
+++ models/product.js
@@ -0,0 +1,12 @@
+class Product {
+  constructor(id, ownerId, title, imegeUrl, description, price) {
+    this.id = id;
+    this.ownerId = ownerId;
+    this.title = title;
+    this.imegeUrl = imegeUrl;
+    this.description = description;
+    this.price = price;
+  }
+}
+
+export default Product;
